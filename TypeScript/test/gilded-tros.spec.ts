48e816e67e698eeaebbc04f7b1224be9a9b50425
Koen De Groot <koendegroot84@gmail.com>
2025-09-27 09:29:00 +0200

    added constants for item names

--- TypeScript/test/gilded-tros.spec.ts
+++ TypeScript/test/gilded-tros.spec.ts
@@ -1,10 +1,11 @@
 import { GildedTros } from "../src/gilded-tros";
 import { Item } from "../src/item";
+import { GILDED_TROS_CONSTANTS } from "./../utils/constants";
 
 describe("GildedTros", () => {
   describe("Regular Items", () => {
     test("should degrade twice as fast after the sell by date", () => {
-      const item: Item = new Item("Regular Item", 0, 10);
+      const item: Item = new Item(GILDED_TROS_CONSTANTS.REGULAR_ITEM, 0, 10);
       const app: GildedTros = new GildedTros([item]);
 
       app.updateQuality();
@@ -14,7 +15,7 @@ describe("GildedTros", () => {
     });
 
     test("should never have negative quality", () => {
-      const item: Item = new Item("Regular Item", 1, 0);
+      const item: Item = new Item(GILDED_TROS_CONSTANTS.REGULAR_ITEM, 1, 0);
       const app: GildedTros = new GildedTros([item]);
 
       app.updateQuality();
@@ -25,7 +26,7 @@ describe("GildedTros", () => {
 
   describe("Good Wine", () => {
     test("should increase in quality the older it gets", () => {
-      const item: Item = new Item("Good Wine", 1, 10);
+      const item: Item = new Item(GILDED_TROS_CONSTANTS.GOOD_WINE, 1, 10);
       const app: GildedTros = new GildedTros([item]);
 
       app.updateQuality();
@@ -35,7 +36,7 @@ describe("GildedTros", () => {
     });
 
     test("should never have quality more than 50", () => {
-      const item: Item = new Item("Good Wine", 1, 50);
+      const item: Item = new Item(GILDED_TROS_CONSTANTS.GOOD_WINE, 1, 50);
       const app: GildedTros = new GildedTros([item]);
 
       app.updateQuality();
@@ -47,7 +48,7 @@ describe("GildedTros", () => {
 
   describe("B-DAWG Keychain", () => {
     test("should never have to be sold or decreases in Quality", () => {
-      const item: Item = new Item("B-DAWG Keychain", 1, 10);
+      const item: Item = new Item(GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN, 1, 10);
       const app: GildedTros = new GildedTros([item]);
 
       app.updateQuality();
@@ -59,8 +60,8 @@ describe("GildedTros", () => {
 
   describe("Backstage passes", () => {
     const backstagePasses = [
-      "Backstage passes for Re:Factor",
-      "Backstage passes for HAXX",
+      GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR,
+      GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX,
     ];
 
     test.each(backstagePasses)(
---
+++ TypeScript/utils/constants.ts
@@ -0,0 +1,10 @@
+export const GILDED_TROS_CONSTANTS = {
+  GOOD_WINE: "Good Wine",
+  BACKSTAGE_PASSES_FOR_HAXX: "Backstage passes for HAXX",
+  BACKSTAGE_PASSES_FOR_RE_FACTOR: "Backstage passes for Re:Factor",
+  B_DAWG_KEYCHAIN: "B-DAWG Keychain",
+  REGULAR_ITEM: "Regular Item",
+  DUPLICATE_CODE: "Duplicate Code",
+  LONG_METHODS: "Long Methods",
+  UGLY_VARIABLE_NAMES: "Ugly Variable Names",
+};
