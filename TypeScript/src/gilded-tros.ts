3dc1acc9a303db631c79b5203152dcc5220aeab4
Koen De Groot <koendegroot84@gmail.com>
2025-09-29 20:55:37 +0200

    removed redundant if check for updateGoodWine because it wil always be false

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -13,32 +13,19 @@ export class GildedTros {
   }
 
   private updateGoodWine(item: Item): void {
-    if (
-      item.name != GILDED_TROS_CONSTANTS.GOOD_WINE &&
-      item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
-      item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
-    ) {
+    if (item.quality < 50) {
+      item.quality += 1;
+
       if (
-        item.quality > 0 &&
-        item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
+        item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR ||
+        item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
       ) {
-        item.quality -= 1;
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality += 1;
+        if (item.sellIn < 11 && item.quality < 50) {
+          item.quality += 1;
+        }
 
-        if (
-          item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR ||
-          item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
-        ) {
-          if (item.sellIn < 11 && item.quality < 50) {
-            item.quality += 1;
-          }
-
-          if (item.sellIn < 6 && item.quality < 50) {
-            item.quality += 1;
-          }
+        if (item.sellIn < 6 && item.quality < 50) {
+          item.quality += 1;
         }
       }
     }
