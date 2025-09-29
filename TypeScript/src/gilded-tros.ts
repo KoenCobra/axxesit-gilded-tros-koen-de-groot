ff2a8c0ce928cf7496e69fc5398dc9072fd5272a
Koen De Groot <koendegroot84@gmail.com>
2025-09-29 21:11:21 +0200

    simplify logic and remove unnecessary condition checks

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -15,30 +15,10 @@ export class GildedTros {
   private updateGoodWine(item: Item): void {
     if (item.quality < 50) item.quality += 1;
 
-    if (item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN) {
-      item.sellIn -= 1;
-    }
+    item.sellIn -= 1;
 
-    if (item.sellIn < 0) {
-      if (item.name != GILDED_TROS_CONSTANTS.GOOD_WINE) {
-        if (
-          item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
-          item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
-        ) {
-          if (
-            item.quality > 0 &&
-            item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
-          ) {
-            item.quality -= 1;
-          }
-        } else {
-          item.quality = 0;
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality += 1;
-        }
-      }
+    if (item.sellIn < 0 && item.quality < 50) {
+      item.quality += 1;
     }
   }
 
