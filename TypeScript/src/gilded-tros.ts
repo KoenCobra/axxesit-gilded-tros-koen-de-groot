5358abd482c80a0834c1516e92c08c43521eb516
Koen De Groot <koendegroot84@gmail.com>
2025-09-29 20:42:21 +0200

    improved readability

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -5,15 +5,11 @@ export class GildedTros {
   constructor(public items: Array<Item>) {}
 
   private updateRegularItem(item: Item): void {
-    if (item.quality > 0) {
-      item.quality -= 1;
-    }
+    if (item.quality > 0) item.quality -= 1;
 
     item.sellIn -= 1;
 
-    if (item.sellIn < 0) {
-      item.quality -= 1;
-    }
+    if (item.sellIn < 0) item.quality -= 1;
   }
 
   private updateGoodWine(item: Item): void {
@@ -254,15 +250,19 @@ export class GildedTros {
         case GILDED_TROS_CONSTANTS.REGULAR_ITEM:
           this.updateRegularItem(item);
           continue;
+
         case GILDED_TROS_CONSTANTS.GOOD_WINE:
           this.updateGoodWine(item);
           continue;
+
         case GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN:
           this.updateBDAWGKeychain(item);
           continue;
+
         case GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR:
           this.updateBackstagePassesForReFactor(item);
           continue;
+
         case GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX:
           this.updateBackstagePassesForHaxx(item);
           continue;
