9d945dc5d250e9d6389954623c6ae0ddaee9f9e6
Koen De Groot <koendegroot84@gmail.com>
2025-09-29 20:07:21 +0200

    added switch statement and placed logic in all the methods before refactoring each on individually

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -4,68 +4,310 @@ import { Item } from "./item";
 export class GildedTros {
   constructor(public items: Array<Item>) {}
 
-  private updateRegularItem(item: Item): void {}
-  private updateGoodWine(item: Item): void {}
-  private updateBDAWGKeychain(item: Item): void {}
-  private updateBackstagePassesForReFactor(item: Item): void {}
-  private updateBackstagePassesForHaxx(item: Item): void {}
-
-  public updateQuality(): void {
-    for (const item of this.items) {
+  private updateRegularItem(item: Item): void {
+    if (
+      item.name != GILDED_TROS_CONSTANTS.GOOD_WINE &&
+      item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
+      item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+    ) {
       if (
-        item.name != GILDED_TROS_CONSTANTS.GOOD_WINE &&
-        item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
-        item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+        item.quality > 0 &&
+        item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
       ) {
+        item.quality -= 1;
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality += 1;
+
         if (
-          item.quality > 0 &&
-          item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
+          item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR ||
+          item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
         ) {
-          item.quality -= 1;
+          if (item.sellIn < 11 && item.quality < 50) {
+            item.quality += 1;
+          }
+
+          if (item.sellIn < 6 && item.quality < 50) {
+            item.quality += 1;
+          }
+        }
+      }
+    }
+
+    if (item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN) {
+      item.sellIn -= 1;
+    }
+
+    if (item.sellIn < 0) {
+      if (item.name != GILDED_TROS_CONSTANTS.GOOD_WINE) {
+        if (
+          item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
+          item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+        ) {
+          if (
+            item.quality > 0 &&
+            item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
+          ) {
+            item.quality -= 1;
+          }
+        } else {
+          item.quality = 0;
         }
       } else {
         if (item.quality < 50) {
           item.quality += 1;
-
-          if (
-            item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR ||
-            item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
-          ) {
-            if (item.sellIn < 11 && item.quality < 50) {
-              item.quality += 1;
-            }
-
-            if (item.sellIn < 6 && item.quality < 50) {
-              item.quality += 1;
-            }
-          }
         }
       }
-
-      if (item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN) {
-        item.sellIn -= 1;
+    }
+  }
+  private updateGoodWine(item: Item): void {
+    if (
+      item.name != GILDED_TROS_CONSTANTS.GOOD_WINE &&
+      item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
+      item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+    ) {
+      if (
+        item.quality > 0 &&
+        item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
+      ) {
+        item.quality -= 1;
       }
+    } else {
+      if (item.quality < 50) {
+        item.quality += 1;
 
-      if (item.sellIn < 0) {
-        if (item.name != GILDED_TROS_CONSTANTS.GOOD_WINE) {
-          if (
-            item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
-            item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
-          ) {
-            if (
-              item.quality > 0 &&
-              item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
-            ) {
-              item.quality -= 1;
-            }
-          } else {
-            item.quality = 0;
+        if (
+          item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR ||
+          item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+        ) {
+          if (item.sellIn < 11 && item.quality < 50) {
+            item.quality += 1;
           }
-        } else {
-          if (item.quality < 50) {
+
+          if (item.sellIn < 6 && item.quality < 50) {
             item.quality += 1;
           }
         }
+      }
+    }
+
+    if (item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN) {
+      item.sellIn -= 1;
+    }
+
+    if (item.sellIn < 0) {
+      if (item.name != GILDED_TROS_CONSTANTS.GOOD_WINE) {
+        if (
+          item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
+          item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+        ) {
+          if (
+            item.quality > 0 &&
+            item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
+          ) {
+            item.quality -= 1;
+          }
+        } else {
+          item.quality = 0;
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality += 1;
+        }
+      }
+    }
+  }
+  private updateBDAWGKeychain(item: Item): void {
+    if (
+      item.name != GILDED_TROS_CONSTANTS.GOOD_WINE &&
+      item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
+      item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+    ) {
+      if (
+        item.quality > 0 &&
+        item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
+      ) {
+        item.quality -= 1;
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality += 1;
+
+        if (
+          item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR ||
+          item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+        ) {
+          if (item.sellIn < 11 && item.quality < 50) {
+            item.quality += 1;
+          }
+
+          if (item.sellIn < 6 && item.quality < 50) {
+            item.quality += 1;
+          }
+        }
+      }
+    }
+
+    if (item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN) {
+      item.sellIn -= 1;
+    }
+
+    if (item.sellIn < 0) {
+      if (item.name != GILDED_TROS_CONSTANTS.GOOD_WINE) {
+        if (
+          item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
+          item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+        ) {
+          if (
+            item.quality > 0 &&
+            item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
+          ) {
+            item.quality -= 1;
+          }
+        } else {
+          item.quality = 0;
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality += 1;
+        }
+      }
+    }
+  }
+  private updateBackstagePassesForReFactor(item: Item): void {
+    if (
+      item.name != GILDED_TROS_CONSTANTS.GOOD_WINE &&
+      item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
+      item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+    ) {
+      if (
+        item.quality > 0 &&
+        item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
+      ) {
+        item.quality -= 1;
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality += 1;
+
+        if (
+          item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR ||
+          item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+        ) {
+          if (item.sellIn < 11 && item.quality < 50) {
+            item.quality += 1;
+          }
+
+          if (item.sellIn < 6 && item.quality < 50) {
+            item.quality += 1;
+          }
+        }
+      }
+    }
+
+    if (item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN) {
+      item.sellIn -= 1;
+    }
+
+    if (item.sellIn < 0) {
+      if (item.name != GILDED_TROS_CONSTANTS.GOOD_WINE) {
+        if (
+          item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
+          item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+        ) {
+          if (
+            item.quality > 0 &&
+            item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
+          ) {
+            item.quality -= 1;
+          }
+        } else {
+          item.quality = 0;
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality += 1;
+        }
+      }
+    }
+  }
+  private updateBackstagePassesForHaxx(item: Item): void {
+    if (
+      item.name != GILDED_TROS_CONSTANTS.GOOD_WINE &&
+      item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
+      item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+    ) {
+      if (
+        item.quality > 0 &&
+        item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
+      ) {
+        item.quality -= 1;
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality += 1;
+
+        if (
+          item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR ||
+          item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+        ) {
+          if (item.sellIn < 11 && item.quality < 50) {
+            item.quality += 1;
+          }
+
+          if (item.sellIn < 6 && item.quality < 50) {
+            item.quality += 1;
+          }
+        }
+      }
+    }
+
+    if (item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN) {
+      item.sellIn -= 1;
+    }
+
+    if (item.sellIn < 0) {
+      if (item.name != GILDED_TROS_CONSTANTS.GOOD_WINE) {
+        if (
+          item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR &&
+          item.name != GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
+        ) {
+          if (
+            item.quality > 0 &&
+            item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN
+          ) {
+            item.quality -= 1;
+          }
+        } else {
+          item.quality = 0;
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality += 1;
+        }
+      }
+    }
+  }
+
+  public updateQuality(): void {
+    for (const item of this.items) {
+      switch (item.name) {
+        case GILDED_TROS_CONSTANTS.REGULAR_ITEM:
+          this.updateRegularItem(item);
+          continue;
+        case GILDED_TROS_CONSTANTS.GOOD_WINE:
+          this.updateGoodWine(item);
+          continue;
+        case GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN:
+          this.updateBDAWGKeychain(item);
+          continue;
+        case GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR:
+          this.updateBackstagePassesForReFactor(item);
+          continue;
+        case GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX:
+          this.updateBackstagePassesForHaxx(item);
+          continue;
       }
     }
   }
