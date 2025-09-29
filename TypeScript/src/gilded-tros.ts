6fb2e3167e0cb507c474964cab514dc548fd480e
Koen De Groot <koendegroot84@gmail.com>
2025-09-29 20:58:37 +0200

    removing redundant condition checks because we are only dealing with wine

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -13,22 +13,7 @@ export class GildedTros {
   }
 
   private updateGoodWine(item: Item): void {
-    if (item.quality < 50) {
-      item.quality += 1;
-
-      if (
-        item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_RE_FACTOR ||
-        item.name == GILDED_TROS_CONSTANTS.BACKSTAGE_PASSES_FOR_HAXX
-      ) {
-        if (item.sellIn < 11 && item.quality < 50) {
-          item.quality += 1;
-        }
-
-        if (item.sellIn < 6 && item.quality < 50) {
-          item.quality += 1;
-        }
-      }
-    }
+    if (item.quality < 50) item.quality += 1;
 
     if (item.name != GILDED_TROS_CONSTANTS.B_DAWG_KEYCHAIN) {
       item.sellIn -= 1;
